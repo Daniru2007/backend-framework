7c75279881de157ad5253a701abdf24573402ba6
Daniru Ayuka <daniruidk@gmail.com>
2026-03-27 01:16:57 +0530

    small changes

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "server/server.h"
 #include "stdio.h"
 
-void func(server *web_server) { printf("FUUUUUCK YOU!\n"); }
+void func(server *web_server) { printf("Testing\n"); }
 void func2(server *web_server) {
-  printf("KISS MY PISS!\n");
+  printf("Testing\n");
   printf("%s\n", web_server->args[0][0]);
   printf("%s\n", web_server->args[0][1]);
 }
@@ -11,7 +11,7 @@ void func2(server *web_server) {
 int main() {
   server *test_server = create_server(8000);
   add_route(test_server, "/something", func);
-  add_route(test_server, "/something/shit", func2);
+  add_route(test_server, "/something/param", func2);
   accept_clients(test_server);
   close_server(test_server);
 }
